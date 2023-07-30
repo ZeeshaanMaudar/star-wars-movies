0cd530277b483a93724f32df4343a18ebf63713f
Zeeshaan Maudarbocus <zeeshaanmaudarbocus@Admins-Air.lan>
2023-07-31 01:33:08 +0200

    fix sort

--- src/shared/helpers/sortArrayByDirectionAndProperty.ts
+++ src/shared/helpers/sortArrayByDirectionAndProperty.ts
@@ -5,11 +5,11 @@ export const sortArrayByDirectionAndProperty = (arr: Movie[], orderDirection: st
     case Order.ASC:
     default:
       return arr.sort((a, b) =>
-        a[orderBy] > b[orderBy] ? 1 : b[orderBy] > a[orderBy] ? -1 : 0
+        b[orderBy] > a[orderBy] ? -1 : 0
       );
     case Order.DESC:
       return arr.sort((a, b) =>
-        a[orderBy] < b[orderBy] ? 1 : b[orderBy] < a[orderBy] ? -1 : 0
+        b[orderBy] < a[orderBy] ? -1 : 0
       );
   }
 };
